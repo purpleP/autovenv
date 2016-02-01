7d1d8eb1d409960a35ddcc9a4b031518612181fe
Михаил Доронин <warrior2031@mail.ru>
2016-02-01 22:48:21 +0300

    WIP

---
+++ new_auto.zsh
@@ -0,0 +1,20 @@
+find_venv_above() {
+#    unsetopt nomatch 2>/dev/null
+    local search_path v
+    search_path=${1:-$HOME}
+    builtin cd $search_path
+    search_path=$(pwd)
+    while [[ ${search_path} = /*/* ]]; do
+        #echo "$search_path"
+        v=$(find $search_path -maxdepth 3 -mindepth 3 -path "*/bin/activate")
+        [[ $v != "" ]] && {
+          printf '%s\n' "$search_path"
+          . $v
+          return 0
+        }
+        search_path=${search_path%/*}
+    done
+    deactivate
+}
+
+alias=find_venv_above
