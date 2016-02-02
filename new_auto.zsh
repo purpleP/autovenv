e1cf0782e28c9a3d015312dbfd827cbaaa2099bb
octogeek <me@octogeek.ru>
2016-02-02 13:14:12 +0300

    Improve find_venv_above
    add 2>/dev/null

--- new_auto.zsh
+++ new_auto.zsh
@@ -1,12 +1,12 @@
 find_venv_above() {
-#    unsetopt nomatch 2>/dev/null
+    unsetopt nomatch 2>/dev/null
     local search_path v
     search_path=${1:-$HOME}
     builtin cd $search_path
     search_path=$(pwd)
     while [[ ${search_path} = /*/* ]]; do
         #echo "$search_path"
-        v=$(find $search_path -maxdepth 3 -mindepth 3 -path "*/bin/activate")
+        v=$(find $search_path -maxdepth 3 -mindepth 3 -path "*/bin/activate" 2>/dev/null)
         [[ $v != "" ]] && {
           #printf '%s\n' "$search_path"
           . $v
@@ -14,8 +14,8 @@ find_venv_above() {
         }
         search_path=${search_path%/*}
     done
-    deactivate
+    deactivate 2>/dev/null
+    return 0
 }
 
 alias cd=find_venv_above
-echo 'lalall'
