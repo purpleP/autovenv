02570cb64d8fbb816bc1559649630fbf582346d9
Michael Doronin <warrior2031@gmail.com>
2016-09-27 10:30:58 +0300

    fixed error that would cause hanging

--- new_auto.zsh
+++ new_auto.zsh
@@ -2,8 +2,8 @@ chpwd() {
     unsetopt nomatch 2>/dev/null
     local search_path v
     search_path=$(pwd)
-    local common_home=$(basename $HOME)
-    while [[ $search_path != $common_home && $search_path != '/' ]]; do
+    local common_home=$(dirname $HOME)
+    while [[ $search_path != $common_home && $search_path != '' ]]; do
         v=$(for d in $search_path/.*/ $search_path/*/; do echo $d; done | xargs -I {} $SHELL -c '[ -e {}bin/activate ] && echo "{}bin/activate"') 2>/dev/null;
         [[ $v != "" ]] && {
           v=$(echo $v | head -n 1)
