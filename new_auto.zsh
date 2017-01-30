921814911133be6147c37ef26db7c21de00a4e2b
Michael Doronin <warrior2031@gmail.com>
2017-01-30 16:10:49 +0300

    added automatic adding folders that contains python packages to pythonpath

--- new_auto.zsh
+++ new_auto.zsh
@@ -1,5 +1,4 @@
-chpwd() {
-    unsetopt nomatch 2>/dev/null
+find_venv() { unsetopt nomatch 2>/dev/null
     local search_path venv_dir
     search_path=$(pwd)
     local common_home=$(dirname $HOME)
@@ -11,3 +10,13 @@ chpwd() {
     deactivate 2>/dev/null
     return 0
 }
+
+has_python_packages() {
+    local package=$(find $(pwd) -maxdepth 1 -type d -exec test -e "{}/__init__.py" \; -print -quit)
+    if [[ -n $package && ":$PYTHONPATH:" != *":$(pwd):"* ]]; then
+        export PYTHONPATH=$PYTHONPATH:$(pwd)
+    fi
+    return 0
+}
+chpwd_functions=(${chpwd_functions[@]} "has_python_packages")
+chpwd_functions=(${chpwd_functions[@]} "find_venv")
